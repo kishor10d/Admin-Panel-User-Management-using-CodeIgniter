401f0f956d45e8c305d6745cfc675122cbbf2b2f
kishor10d <kishor10d@gmail.com>
2016-11-15 22:13:02 +0530

    404 page added, route added

---
+++ application/controllers/error.php
@@ -0,0 +1,46 @@
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Class : Login (LoginController)
+ * Login class to control to authenticate user credentials and starts user's session.
+ * @author : Kishor Mali
+ * @version : 1.1
+ * @since : 15 November 2016
+ */
+class Error extends CI_Controller
+{
+    /**
+     * This is default constructor of the class
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Index Page for this controller.
+     */
+    public function index()
+    {
+        $this->isLoggedIn();
+    }
+    
+    /**
+     * This function used to check the user is logged in or not
+     */
+    function isLoggedIn()
+    {
+        $isLoggedIn = $this->session->userdata('isLoggedIn');
+        
+        if(!isset($isLoggedIn) || $isLoggedIn != TRUE)
+        {
+            $this->load->view('login');
+        }
+        else
+        {
+            redirect('pageNotFound');
+        }
+    }
+}
+
+?>
