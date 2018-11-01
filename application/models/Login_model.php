4bc0839ea48dd69818de3c8080e8267594cf06da
Kishor Mali <kishor10d@gmail.com>
2018-11-01 10:17:49 +0530

    class level comments added

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -1,5 +1,12 @@
 <?php if(!defined('BASEPATH')) exit('No direct script access allowed');
 
+/**
+ * Class : Login_model (Login Model)
+ * Login model class to get to authenticate user credentials 
+ * @author : Kishor Mali
+ * @version : 1.1
+ * @since : 15 November 2016
+ */
 class Login_model extends CI_Model
 {
     
--- application/models/User_model.php
+++ application/models/User_model.php
@@ -1,5 +1,12 @@
 <?php if(!defined('BASEPATH')) exit('No direct script access allowed');
 
+/**
+ * Class : User_model (User Model)
+ * User model class to get to handle user related data 
+ * @author : Kishor Mali
+ * @version : 1.1
+ * @since : 15 November 2016
+ */
 class User_model extends CI_Model
 {
     /**
