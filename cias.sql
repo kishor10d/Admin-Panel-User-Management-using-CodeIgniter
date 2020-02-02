59a181c15321f393c87eff6c28261133689329b6
Daniel <50356015+danny007in@users.noreply.github.com>
2020-02-02 12:49:37 +0530

    group table added

--- cias.sql
+++ cias.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 18, 2018 at 08:21 AM
--- Server version: 10.1.16-MariaDB
--- PHP Version: 5.6.24
+-- Host: 127.0.0.1:3306
+-- Generation Time: Feb 02, 2020 at 07:17 AM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.4.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -26,13 +28,44 @@ SET time_zone = "+00:00";
 -- Table structure for table `ci_sessions`
 --
 
-CREATE TABLE `ci_sessions` (
+DROP TABLE IF EXISTS `ci_sessions`;
+CREATE TABLE IF NOT EXISTS `ci_sessions` (
   `session_id` varchar(40) NOT NULL DEFAULT '0',
   `ip_address` varchar(45) NOT NULL DEFAULT '0',
   `user_agent` varchar(120) NOT NULL,
-  `last_activity` int(10) UNSIGNED NOT NULL DEFAULT '0',
-  `user_data` text NOT NULL
+  `last_activity` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `user_data` text NOT NULL,
+  PRIMARY KEY (`session_id`),
+  KEY `last_activity_idx` (`last_activity`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_groups`
+--
+
+DROP TABLE IF EXISTS `tbl_groups`;
+CREATE TABLE IF NOT EXISTS `tbl_groups` (
+  `group_id` int(8) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `group_name` varchar(64) NOT NULL,
+  `permission` text NOT NULL,
+  `created_at` datetime NOT NULL,
+  `created_by` int(8) UNSIGNED NOT NULL,
+  `modified_at` datetime DEFAULT NULL,
+  `modified_by` int(8) UNSIGNED DEFAULT NULL,
+  `status` tinyint(2) UNSIGNED NOT NULL COMMENT '0 - Enable | 1 - Disable',
+  PRIMARY KEY (`group_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_groups`
+--
+
+INSERT INTO `tbl_groups` (`group_id`, `group_name`, `permission`, `created_at`, `created_by`, `modified_at`, `modified_by`, `status`) VALUES
+(1, 'System Administrator', 'a:11:{i:0;s:10:\"createUser\";i:1;s:10:\"updateUser\";i:2;s:8:\"viewUser\";i:3;s:10:\"deleteUser\";i:4;s:11:\"createGroup\";i:5;s:11:\"updateGroup\";i:6;s:9:\"viewGroup\";i:7;s:11:\"deleteGroup\";i:8;s:11:\"viewProfile\";i:9;s:13:\"updateSetting\";i:10;s:16:\"viewLoginHistory\";}', '0000-00-00 00:00:00', 0, '2020-02-02 07:03:11', 1, 0),
+(2, 'Manager', 'a:7:{i:0;s:10:\"createUser\";i:1;s:10:\"updateUser\";i:2;s:8:\"viewUser\";i:3;s:11:\"createGroup\";i:4;s:9:\"viewGroup\";i:5;s:11:\"viewProfile\";i:6;s:13:\"updateSetting\";}', '2020-02-02 03:36:38', 1, '2020-02-02 07:03:11', 1, 0),
+(3, 'Employee', 'a:3:{i:0;s:10:\"createUser\";i:1;s:8:\"viewUser\";i:2;s:11:\"viewProfile\";}', '2020-02-02 03:38:02', 1, '2020-02-02 06:34:03', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -40,16 +73,18 @@ CREATE TABLE `ci_sessions` (
 -- Table structure for table `tbl_last_login`
 --
 
-CREATE TABLE `tbl_last_login` (
-  `id` bigint(20) NOT NULL,
+DROP TABLE IF EXISTS `tbl_last_login`;
+CREATE TABLE IF NOT EXISTS `tbl_last_login` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `userId` bigint(20) NOT NULL,
   `sessionData` varchar(2048) NOT NULL,
   `machineIp` varchar(1024) NOT NULL,
   `userAgent` varchar(128) NOT NULL,
   `agentString` varchar(1024) NOT NULL,
   `platform` varchar(128) NOT NULL,
-  `createdDtm` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `createdDtm` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -57,17 +92,19 @@ CREATE TABLE `tbl_last_login` (
 -- Table structure for table `tbl_reset_password`
 --
 
-CREATE TABLE `tbl_reset_password` (
-  `id` bigint(20) NOT NULL,
+DROP TABLE IF EXISTS `tbl_reset_password`;
+CREATE TABLE IF NOT EXISTS `tbl_reset_password` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `email` varchar(128) NOT NULL,
   `activation_id` varchar(32) NOT NULL,
   `agent` varchar(512) NOT NULL,
   `client_ip` varchar(32) NOT NULL,
-  `isDeleted` tinyint(4) NOT NULL DEFAULT '0',
-  `createdBy` bigint(20) NOT NULL DEFAULT '1',
+  `isDeleted` tinyint(4) NOT NULL DEFAULT 0,
+  `createdBy` bigint(20) NOT NULL DEFAULT 1,
   `createdDtm` datetime NOT NULL,
   `updatedBy` bigint(20) DEFAULT NULL,
-  `updatedDtm` datetime DEFAULT NULL
+  `updatedDtm` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -76,10 +113,12 @@ CREATE TABLE `tbl_reset_password` (
 -- Table structure for table `tbl_roles`
 --
 
-CREATE TABLE `tbl_roles` (
-  `roleId` tinyint(4) NOT NULL COMMENT 'role id',
-  `role` varchar(50) NOT NULL COMMENT 'role text'
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `tbl_roles`;
+CREATE TABLE IF NOT EXISTS `tbl_roles` (
+  `roleId` tinyint(4) NOT NULL AUTO_INCREMENT COMMENT 'role id',
+  `role` varchar(50) NOT NULL COMMENT 'role text',
+  PRIMARY KEY (`roleId`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `tbl_roles`
@@ -96,19 +135,21 @@ INSERT INTO `tbl_roles` (`roleId`, `role`) VALUES
 -- Table structure for table `tbl_users`
 --
 
-CREATE TABLE `tbl_users` (
-  `userId` int(11) NOT NULL,
+DROP TABLE IF EXISTS `tbl_users`;
+CREATE TABLE IF NOT EXISTS `tbl_users` (
+  `userId` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(128) NOT NULL COMMENT 'login email',
   `password` varchar(128) NOT NULL COMMENT 'hashed login password',
   `name` varchar(128) DEFAULT NULL COMMENT 'full name of user',
   `mobile` varchar(20) DEFAULT NULL,
   `roleId` tinyint(4) NOT NULL,
-  `isDeleted` tinyint(4) NOT NULL DEFAULT '0',
+  `isDeleted` tinyint(4) NOT NULL DEFAULT 0,
   `createdBy` int(11) NOT NULL,
   `createdDtm` datetime NOT NULL,
   `updatedBy` int(11) DEFAULT NULL,
-  `updatedDtm` datetime DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `updatedDtm` datetime DEFAULT NULL,
+  PRIMARY KEY (`userId`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `tbl_users`
@@ -118,66 +159,8 @@ INSERT INTO `tbl_users` (`userId`, `email`, `password`, `name`, `mobile`, `roleI
 (1, 'admin@example.com', '$2y$10$6NOKhXKiR2SAgpFF2WpCkuRgYKlSqFJaqM0NgIM3PT1gKHEM5/SM6', 'System Administrator', '9890098900', 1, 0, 0, '2015-07-01 18:56:49', 1, '2018-01-05 05:56:34'),
 (2, 'manager@example.com', '$2y$10$quODe6vkNma30rcxbAHbYuKYAZQqUaflBgc4YpV9/90ywd.5Koklm', 'Manager', '9890098900', 2, 0, 1, '2016-12-09 17:49:56', 1, '2018-01-12 07:22:11'),
 (3, 'employee@example.com', '$2y$10$UYsH1G7MkDg1cutOdgl2Q.ZbXjyX.CSjsdgQKvGzAgl60RXZxpB5u', 'Employee', '9890098900', 3, 0, 1, '2016-12-09 17:50:22', 3, '2018-01-04 07:58:28');
+COMMIT;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `ci_sessions`
---
-ALTER TABLE `ci_sessions`
-  ADD PRIMARY KEY (`session_id`),
-  ADD KEY `last_activity_idx` (`last_activity`);
-
---
--- Indexes for table `tbl_last_login`
---
-ALTER TABLE `tbl_last_login`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `tbl_reset_password`
---
-ALTER TABLE `tbl_reset_password`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `tbl_roles`
---
-ALTER TABLE `tbl_roles`
-  ADD PRIMARY KEY (`roleId`);
-
---
--- Indexes for table `tbl_users`
---
-ALTER TABLE `tbl_users`
-  ADD PRIMARY KEY (`userId`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `tbl_last_login`
---
-ALTER TABLE `tbl_last_login`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `tbl_reset_password`
---
-ALTER TABLE `tbl_reset_password`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `tbl_roles`
---
-ALTER TABLE `tbl_roles`
-  MODIFY `roleId` tinyint(4) NOT NULL AUTO_INCREMENT COMMENT 'role id', AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `tbl_users`
---
-ALTER TABLE `tbl_users`
-  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
