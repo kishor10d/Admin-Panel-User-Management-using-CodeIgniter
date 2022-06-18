0983fa62079196dc8a1fb37e7a49e5b6df5e530c
kishor10d <kishor10d@gmail.com>
2022-06-19 00:27:24 +0530

    Task module CRUD complete, db file cias.sql updated

---
+++ application/models/Task_model.php
@@ -0,0 +1,100 @@
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Class : Task_model (Task Model)
+ * Task model class to get to handle task related data 
+ * @author : Kishor Mali
+ * @version : 1.5
+ * @since : 18 Jun 2022
+ */
+class Task_model extends CI_Model
+{
+    /**
+     * This function is used to get the task listing count
+     * @param string $searchText : This is optional search text
+     * @return number $count : This is row count
+     */
+    function taskListingCount($searchText = '')
+    {
+        $this->db->select('BaseTbl.taskId, BaseTbl.taskTitle, BaseTbl.description, BaseTbl.createdDtm');
+        $this->db->from('tbl_task as BaseTbl');
+        if(!empty($searchText)) {
+            $likeCriteria = "(BaseTbl.taskTitle LIKE '%".$searchText."%')";
+            $this->db->where($likeCriteria);
+        }
+        $this->db->where('BaseTbl.isDeleted', 0);
+        $query = $this->db->get();
+        
+        return $query->num_rows();
+    }
+    
+    /**
+     * This function is used to get the task listing count
+     * @param string $searchText : This is optional search text
+     * @param number $page : This is pagination offset
+     * @param number $segment : This is pagination limit
+     * @return array $result : This is result
+     */
+    function taskListing($searchText = '', $page, $segment)
+    {
+        $this->db->select('BaseTbl.taskId, BaseTbl.taskTitle, BaseTbl.description, BaseTbl.createdDtm');
+        $this->db->from('tbl_task as BaseTbl');
+        if(!empty($searchText)) {
+            $likeCriteria = "(BaseTbl.taskTitle LIKE '%".$searchText."%')";
+            $this->db->where($likeCriteria);
+        }
+        $this->db->where('BaseTbl.isDeleted', 0);
+        $this->db->order_by('BaseTbl.taskId', 'DESC');
+        $this->db->limit($page, $segment);
+        $query = $this->db->get();
+        
+        $result = $query->result();        
+        return $result;
+    }
+    
+    /**
+     * This function is used to add new task to system
+     * @return number $insert_id : This is last inserted id
+     */
+    function addNewTask($taskInfo)
+    {
+        $this->db->trans_start();
+        $this->db->insert('tbl_task', $taskInfo);
+        
+        $insert_id = $this->db->insert_id();
+        
+        $this->db->trans_complete();
+        
+        return $insert_id;
+    }
+    
+    /**
+     * This function used to get task information by id
+     * @param number $taskId : This is task id
+     * @return array $result : This is task information
+     */
+    function getTaskInfo($taskId)
+    {
+        $this->db->select('taskId, taskTitle, description');
+        $this->db->from('tbl_task');
+        $this->db->where('taskId', $taskId);
+        $this->db->where('isDeleted', 0);
+        $query = $this->db->get();
+        
+        return $query->row();
+    }
+    
+    
+    /**
+     * This function is used to update the task information
+     * @param array $taskInfo : This is task updated information
+     * @param number $taskId : This is task id
+     */
+    function editTask($taskInfo, $taskId)
+    {
+        $this->db->where('taskId', $taskId);
+        $this->db->update('tbl_task', $taskInfo);
+        
+        return TRUE;
+    }
+}
---
+++ application/views/task/edit.php
@@ -0,0 +1,87 @@
+<?php
+$taskId = $taskInfo->taskId;
+$taskTitle = $taskInfo->taskTitle;
+$description = $taskInfo->description;
+?>
+
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        <i class="fa fa-user-circle-o" aria-hidden="true"></i> Task Management
+        <small>Add / Edit Task</small>
+      </h1>
+    </section>
+    
+    <section class="content">
+    
+        <div class="row">
+            <!-- left column -->
+            <div class="col-md-8">
+              <!-- general form elements -->
+                
+                <div class="box box-primary">
+                    <div class="box-header">
+                        <h3 class="box-title">Enter Task Details</h3>
+                    </div><!-- /.box-header -->
+                    <!-- form start -->
+                    
+                    <form role="form" action="<?php echo base_url() ?>task/editTask" method="post" id="editTask" role="form">
+                        <div class="box-body">
+                        <div class="row">
+                                <div class="col-md-6">                                
+                                    <div class="form-group">
+                                        <label for="taskTitle">Task Title</label>
+                                        <input type="text" class="form-control required" value="<?php echo $taskTitle; ?>" id="taskTitle" name="taskTitle" maxlength="256" />
+                                        <input type="hidden" value="<?php echo $taskId; ?>" name="taskId" id="taskId" />
+                                    </div>
+                                    
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="description">Description</label>
+                                        <textarea class="form-control required" id="description" name="description"><?php echo $description; ?></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                        </div><!-- /.box-body -->
+    
+                        <div class="box-footer">
+                            <input type="submit" class="btn btn-primary" value="Submit" />
+                            <input type="reset" class="btn btn-default" value="Reset" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <?php
+                    $this->load->helper('form');
+                    $error = $this->session->flashdata('error');
+                    if($error)
+                    {
+                ?>
+                <div class="alert alert-danger alert-dismissable">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                    <?php echo $this->session->flashdata('error'); ?>                    
+                </div>
+                <?php } ?>
+                <?php  
+                    $success = $this->session->flashdata('success');
+                    if($success)
+                    {
+                ?>
+                <div class="alert alert-success alert-dismissable">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                    <?php echo $this->session->flashdata('success'); ?>
+                </div>
+                <?php } ?>
+                
+                <div class="row">
+                    <div class="col-md-12">
+                        <?php echo validation_errors('<div class="alert alert-danger alert-dismissable">', ' <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button></div>'); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
